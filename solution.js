9628347186861be896a0434636dddda2f988ab1c
chris <chrishethertonweb@gmail.com>
2022-07-12 18:22:25 +0200

    ex 3 done

--- solution.js
+++ solution.js
@@ -1,3 +1,11 @@
 for (let i = 1; i < 6; i++) {
   console.log("There are", i, " bottles of beer on the table.");
 }
+
+for (let i = 0; i < 21; i++) {
+  if (i % 2 == 0) {
+    console.log(i + " is even!");
+  } else {
+    console.log(i + " is odd!");
+  }
+}
