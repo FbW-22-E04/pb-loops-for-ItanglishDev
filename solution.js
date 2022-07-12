f408c6533b6d33d6ee4ad5394df1c3763ddecbc4
chris <chrishethertonweb@gmail.com>
2022-07-12 18:45:37 +0200

    ex 5 done

--- solution.js
+++ solution.js
@@ -9,3 +9,18 @@ for (let i = 0; i < 21; i++) {
     console.log(i + " is odd!");
   }
 }
+
+for (let i = 0; i < 11; i++) {
+  //   for (let i = 0; i < 11; i++) {}
+  console.log(i * 9);
+}
+
+for (let i = 0; i < 101; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz", i);
+  } else if (i % 3 == 0) {
+    console.log("Fizz", i);
+  } else if (i % 5 == 0) {
+    console.log("Buzz", i);
+  }
+}
